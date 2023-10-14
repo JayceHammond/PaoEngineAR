332b936d6ebf6a3b2cded45b7701dace3fdf18aa
JayceHammond <hjayce6@gmail.com>
2023-10-14 17:57:55 -0500

    initial commit

---
+++ Assets/Scripts/AdjustBoxCube.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+
+public class AdjustBoxCube : MonoBehaviour
+{
+    public Camera ARCam;
+
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(ARCam.transform.position.x + 8.6f, ARCam.transform.position.y, transform.position.z);
+    }
+}
